737d4680534b5f2eea0fa44ef7cd8cc4c61a18f2
Joshua Swiss <swissjoshua@gmail.com>
2015-08-22 13:45:44 +0100

    wrote pseudocode and added function layout

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
+var typingGame = {};
+
+typingGame.loremFactory = function(level, paragraphs){
+  this.level = level;
+  this.paragraphs = paragraphs;
+};
+
+typingGameLevel1 = new typingGame.loremFactory('Level 1', //arrays);
+typingGameLevel2 = new typingGame.loremFactory('Level 2', //arrays);
+
+typingGame.splitParagraphText = function(typingGame.loremFactory.paragraphs){
+  //splits string into array of strings of words/spaces
+}
+
+typingGame.getCharacters = function(keyboard){
+  //gets typed characters
+}
+
+typingGame.updateTypingHTML = function(){
+  //compares typed letters to split paragraphs
+}
+
+typingGame.timer = function(){
+  //create a timer
+  //show it in the footer left box
+  //use it to trigger typingGame.animatedEndOfGameBox
+}
+
+typingGame.updateScores = function(){
+  //update fields in footer left and popup box
+}
+
+typingGame.animatedEndOfGameBox = function(){
+  //write a function to animate when the game is over
+}
+
+typingGame.switchPlayers = function(){
+  //switch from player one to player 2
+}
+
 $(document).ready(function(){
-
-
+  //add callback functions for event listeners here
 });
